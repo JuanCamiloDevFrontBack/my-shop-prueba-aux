cb718afd2e3f012ea91410a5b3049a8f913a0c93
JuanCamilo9915 <jcamiloduque0215@gmail.com>
2024-04-24 23:11:40 -0500

    Se eliminan algunos comentarios del servicio http-api.

--- src/app/core/services/http-api.service.ts
+++ src/app/core/services/http-api.service.ts
@@ -1,36 +1,29 @@
-// import { HttpClient } from '@angular/common/http';
 import { Injectable, inject } from '@angular/core';
 import { BillF, Producto } from '../interfaces/producto';
 import { ProductService } from './product.service';
-// import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HttpApiService {
 
-  // private readonly http = inject(HttpClient);
   private readonly httpFake = inject(ProductService);
 
   constructor() { }
 
   getStockProductHttp(): Promise<unknown> {
-    // this.http.get('url-api/endpoint');
     return this.httpFake.getRegistersHttpFake();
   }
 
   createProductStockHttp(newProduct: Producto): Promise<unknown> {
-    // this.http.post('url-api/endpoint', editProduct);
     return this.httpFake.postCreateRegister(newProduct);
   }
 
   updateProductStockHttp(editProduct: Producto): Promise<unknown> {
-    // this.http.put('url-api/endpoint', editProduct);
     return this.httpFake.postEditRegister(editProduct);
   }
 
   deleteProductStockHttp(deleteProducts: Producto[]): Promise<unknown> {
-    // this.http.delete('url-api/endpoint', editProduct);
     const arrId: number[] = [];
     deleteProducts.forEach(prod => {
       arrId.push(prod.id);
@@ -39,12 +32,10 @@ export class HttpApiService {
   }
 
   getBillHttp(): Promise<unknown> {
-    // this.http.get('url-api/endpoint');
     return this.httpFake.getRegistersBillHttpFake();
   }
 
   updateBillHttp(addProduct: BillF): Promise<unknown> {
-    // this.http.put('url-api/endpoint', addProduct);
     return this.httpFake.postAddRegisterBill(addProduct);
   }
 
